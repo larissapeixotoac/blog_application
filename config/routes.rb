e98a2d3944a71ec8e51755ba1b23088353cf785c
RissaCP <larissapacavalcante@gmail.com>
2023-08-27 21:35:17 -0300

    Adding user sign in, login and logout, dropdow for the user, and user name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
+
   resources :posts
   get 'about', to: 'pages#about'
-  get "posts", to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
